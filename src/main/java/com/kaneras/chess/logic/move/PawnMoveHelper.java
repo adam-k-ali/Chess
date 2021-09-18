28efee8197b6fe4da83f41341f665231a43b9b50
Adam Ali <adamka2000@outlook.com>
2021-09-18 20:52:00 +0100

    Bug fixes for validation of pawn moves

--- src/main/java/com/kaneras/chess/logic/move/PawnMoveHelper.java
+++ src/main/java/com/kaneras/chess/logic/move/PawnMoveHelper.java
@@ -12,23 +12,31 @@ public class PawnMoveHelper extends MoveHelper {
     @Override
     public boolean isValidMove() {
         // On the first move, a pawn can move 2 spaces forward.
-        if ((startPiece.getOwner() == Game.Player.WHITE && startY == 6 || startPiece.getOwner() == Game.Player.BLACK && startY == 1) && movedNForward(2)) {
+        if (isAtStart() && movedNForward(2) && Math.abs(startX - finishX) == 0) {
             return true;
         }
 
+        // Take the opposite team's piece.
         if (isMoveDiagonal() && getDistanceMoved() == 1) {
             return oppositeTeams() && finishPiece != null;
         }
 
         // Checks for if the pawn has moved 1 space forward.
-        if (movedNForward(1)) {
+        if (movedNForward(1) && Math.abs(startX - finishX) == 0) {
             // Team that moved can take piece if there is one of getTile(x, y).getPiece() the opposite team.
-            if (movedNHorizontal(1) && oppositeTeams() && finishPiece != null) {
+            if (isMoveDiagonal() || moveClashes()) {
+                return false;
+            }
+            if (finishTile.getPiece() == null) {
                 return true;
-            } else return finishPiece == null; // If the new position is empty.
+            }
         }
 
         return false;
+    }
+
+    private boolean isAtStart() {
+        return startPiece.getOwner() == Game.Player.WHITE && startY == 6 || startPiece.getOwner() == Game.Player.BLACK && startY == 1;
     }
 
 }
